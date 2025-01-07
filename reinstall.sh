919da273b263672cd6536837af51ccacd5cdee5c
_O <63698224+iceofrace@users.noreply.github.com>
2025-01-07 11:29:31 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,12 +3,12 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://jihulab.com/bin456789/reinstall/-/raw/main
+confhome=https://raw.githubusercontent.com/iceofrace/reinstall/main
+confhome_cn=https://raw.githubusercontent.com/iceofrace/reinstall/main
 # confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
 
 # 默认密码
-DEFAULT_PASSWORD=123@@@
+DEFAULT_PASSWORD=123@@@Qq
 
 # 用于判断 reinstall.sh 和 trans.sh 是否兼容
 SCRIPT_VERSION=4BACD833-A585-23BA-6CBB-9AA4E08E0002
